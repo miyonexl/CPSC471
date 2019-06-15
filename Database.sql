6215ae7675d9b680a3714309e56ed761a077a79e
Xia Liu <xialiu1998@gmail.com>
2019-06-15 00:07:46 -0600

    updated doc, added login db

---
+++ Database.sql
@@ -0,0 +1,21 @@
+﻿# Host: localhost:3308  (Version 5.5.62)
+# Date: 2019-06-15 00:03:12
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(12) NOT NULL DEFAULT 'username',
+  `password` varchar(30) NOT NULL DEFAULT 'password',
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "users"
+#
+
+INSERT INTO `users` VALUES ('employee','employeepw');
--- README.md
+++
@@ -1,2 +0,0 @@
-# CPSC471
-CPSC 471 Spring 2019 Group Project
