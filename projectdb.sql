1ac92c77cdb7f14878975a695c60fac89c977d77
JZuCuadra <jesusandresck@gmail.com>
2019-06-16 14:51:45 -0600

    Fixed Populated DB

--- projectdb.sql
+++ projectdb.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version 5.7.17-log)
-# Date: 2019-06-16 13:51:43
+# Date: 2019-06-16 14:51:00
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -52,7 +52,7 @@ CREATE TABLE `employee` (
   `MName` varchar(255) DEFAULT NULL,
   `LName` varchar(255) NOT NULL DEFAULT 'LName',
   `Salary` decimal(10,2) unsigned DEFAULT '0.00',
-  `ManagerID` int(11) unsigned DEFAULT '0',
+  `ManagerID` int(11) unsigned DEFAULT NULL,
   `DeptName` varchar(255) NOT NULL DEFAULT 'DName',
   `CompName` varchar(255) NOT NULL DEFAULT 'CompName',
   `EmpStartDate` date DEFAULT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE `employee` (
 # Data for table "employee"
 #
 
-INSERT INTO `employee` VALUES (1,'FName1','MName1','LName1',50000.00,0,'Dept1','CompanyI','2019-06-15','m','pass1'),(2,'FName2','MName2','LName2',20000.00,1,'Dept1','CompanyI','2019-06-16','u','pass2'),(3,'FName3',NULL,'LName3',0.00,1,'Dept2','CompanyI',NULL,'u','pass3');
+INSERT INTO `employee` VALUES (1,'FName1','MName1','LName1',50000.00,0,'Dept1','CompanyI','2019-06-15','m','pass1'),(2,'FName2','MName2','LName2',20000.00,1,'Dept1','CompanyI','2019-06-16','u','pass2'),(3,'FName3',NULL,'LName3',0.00,1,'Dept2','CompanyI',NULL,'u','pass3'),(4,'FName1','MName1','LName1',50000.00,NULL,'Dept2','CompanyI','2019-06-16','m','pass4');
 
 #
 # Structure for table "manager"
